c38bb2bc86d7e5e3e46ab4b7dbf3f9988f79f4b3
rof1yev <rofiyevdilshod@gmail.com>
2024-09-03 19:01:16 +0500

    next.config remove change

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
